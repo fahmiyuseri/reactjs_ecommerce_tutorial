77783b4b7a4c5ed33fc8793985021c92bc45a1ad
fahmi <fahmiyuseri@gmail.com>
2022-01-21 16:15:19 +0800

    Product Detail page

--- src/screen/ProductScreen.js
+++ src/screen/ProductScreen.js
@@ -1,12 +1,121 @@
-import React from "react";
+import React, { useReducer, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import logger from "use-reducer-logger";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
+import ListGroup from "react-bootstrap/ListGroup";
+import Rating from "../components/Rating";
+import Button from "react-bootstrap/Button";
+import { Helmet } from "react-helmet-async";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, product: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 export default function ProductScreen() {
+  const [{ loading, error, product }, dispatch] = useReducer(logger(reducer), {
+    loading: true,
+    error: "",
+    products: [],
+  });
+  // const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+    };
+    fetchData();
+  }, []);
+
   const params = useParams();
   const { slug } = params;
   return (
     <div>
-      <h1>{slug}</h1>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <div>
+          <Row>
+            <Col md={6}>
+              <img
+                className='img-large'
+                src={product.image}
+                alt={product.name}
+              ></img>
+            </Col>
+            <Col md={3}>
+              <ListGroup>
+                <ListGroup.Item>
+                  <Helmet>
+                    <title>{product.name}</title>
+                  </Helmet>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                  ></Rating>{" "}
+                </ListGroup.Item>
+                <ListGroup.Item>Price : RM {product.price}</ListGroup.Item>
+                <ListGroup.Item>
+                  Description:<p>{product.description}</p>
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={3}>
+              <Card>
+                <Card.Body>
+                  <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Price</Col>
+                        <Col>RM {product.price}</Col>
+                      </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Status</Col>
+                        <Col>
+                          {product.countStock > 0 ? (
+                            <Badge bg='success'>In Stock</Badge>
+                          ) : (
+                            <Badge bg='danger'>Out of stock</Badge>
+                          )}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                    {product.countStock > 0 && (
+                      <div className='d-grid'>
+                        <Button variant='primary' style={{ marginTop: 10 }}>
+                          Add to Cart
+                        </Button>
+                      </div>
+                    )}
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      )}{" "}
     </div>
   );
 }
